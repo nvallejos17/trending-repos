7dc58aa9e5c272d3620f4c49ebed3785a17f230a
Ignacio Vallejos <nacho.vallejos17@outlook.com>
2022-04-12 13:54:59 -0300

    Add pagination to Repos component

--- src/components/Repos/Repos.component.tsx
+++ src/components/Repos/Repos.component.tsx
@@ -1,22 +1,34 @@
+import { useState } from 'react'
 import IRepo from '../../types/IRepo.type'
 import RepoItem from '../RepoItem/RepoItem.component'
-import { ReposWrapper } from './Repos.styled'
+import { ReposList, ReposLoadMoreButton, ReposWrapper } from './Repos.styled'
 
 interface IReposProps {
   repos: IRepo[]
 }
 
 const Repos = ({ repos }: IReposProps) => {
+  const reposPerPage = 4
+  const [currentPage, setCurrentPage] = useState(1)
+  const reposToShow = repos.slice(0, reposPerPage * currentPage)
+
   return (
     <ReposWrapper>
       {repos?.length > 0 ? (
-        repos.map((repo: IRepo) => (
-          <li key={repo.id}>
-            <RepoItem repo={repo} />
-          </li>
-        ))
+        <ReposList>
+          {reposToShow.map((repo: IRepo) => (
+            <li key={repo.id}>
+              <RepoItem repo={repo} />
+            </li>
+          ))}
+        </ReposList>
       ) : (
-        <div>Ops! There're no repositories</div>
+        <div>There're no repositories</div>
+      )}
+      {repos.length > reposPerPage && reposToShow.length < repos.length && (
+        <ReposLoadMoreButton onClick={() => setCurrentPage(currentPage + 1)}>
+          Load {reposPerPage} more repos
+        </ReposLoadMoreButton>
       )}
     </ReposWrapper>
   )
--- src/components/Repos/Repos.styled.tsx
+++ src/components/Repos/Repos.styled.tsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components'
+import Button from '../Button/Button.component'
 
-export const ReposWrapper = styled.ul`
+export const ReposWrapper = styled.div``
+
+export const ReposList = styled.ul`
   list-style: none;
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(calc(320px), 1fr));
@@ -9,4 +12,9 @@ export const ReposWrapper = styled.ul`
   @media (max-width: 320px) {
     grid-template-columns: none;
   }
+`
+
+export const ReposLoadMoreButton = styled(Button)`
+  width: 100%;
+  margin-top: 2rem;
 `
