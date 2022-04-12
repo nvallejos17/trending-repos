8057f8140dcd3e76d6ae7e49721eaf8724c2d7f8
Ignacio Vallejos <nacho.vallejos17@outlook.com>
2022-04-12 13:27:38 -0300

    Fix some styles

--- src/components/Footer/Footer.styled.tsx
+++ src/components/Footer/Footer.styled.tsx
@@ -2,7 +2,7 @@ import styled, { css } from 'styled-components'
 import Container from '../Container/Container.component'
 
 export const FooterWrapper = styled.footer(
-  ({ theme }: any) => css`
+  ({ theme }) => css`
     padding: 1rem 0;
     background-color: ${theme.colors.bgFooter};
   `
--- src/components/Section/Section.styled.tsx
+++ src/components/Section/Section.styled.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const SectionWrapper = styled.section`
-  min-height: calc(100vh - 60.5px - 51.5px);
+  min-height: calc(100vh - 50.5px - 50.5px);
   padding: 2rem 0;
 `
 
