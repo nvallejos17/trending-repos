e8104eecddfb17e26a8108d57e409af707fde068
Ignacio Vallejos <nacho.vallejos17@outlook.com>
2022-04-12 13:09:19 -0300

    Change Button border-radius

--- src/components/Button/Button.styled.tsx
+++ src/components/Button/Button.styled.tsx
@@ -3,7 +3,7 @@ import styled, { css } from 'styled-components'
 export const ButtonWrapper = styled.button(
   ({ theme }) => css`
     padding: 0.5rem 1rem;
-    border-radius: 0.5rem;
+    border-radius: 0.25rem;
     color: ${theme.colors.fontButton};
     border: 1px solid ${theme.colors.borderButton};
     background-color: ${theme.colors.bgButton};
